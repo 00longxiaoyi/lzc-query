748f6644c4b8f4f4110f4f9a4f5c2d8183b9b37d
longxiaoyi <00longxiaoyi@gmail.com>
2025-10-01 13:59:33 +0800

    add: 添加全局变量保存当前获取到的所有图片的基础信息

--- backend/App/utils/FormatText.py
+++ backend/App/utils/FormatText.py
@@ -1,6 +1,11 @@
 # 文本写入
+import json
 import os
+from typing import List
+from struc.software import TempPicInfoStruc
 from utils.getUserInfo import GetDevUserInfo
+
+FILE_DIR = "./files/pics/"
 
 def FormatText(fileName: str, dirname:str, content: dict, type: str):
     # 格式化
@@ -16,8 +21,26 @@ def FormatText(fileName: str, dirname:str, content: dict, type: str):
                 result += f" - 玩机攻略：{item[0]} \n "
 
         result += "\n"
-    if not (os.path.exists(f"./pics/{dirname}")):
-        os.makedirs(f"./pics/{dirname}")
-    with open(f"./pics/{dirname}/{fileName}", 'w', encoding="utf-8") as f:
+    if not (os.path.exists(f"{FILE_DIR}{dirname}")):
+        os.makedirs(f"{FILE_DIR}{dirname}")
+    with open(f"{FILE_DIR}{dirname}/{fileName}", 'w', encoding="utf-8") as f:
         f.write(result)
     return result
+
+def SaveFormatTempPicsInfo(info: List[TempPicInfoStruc]):
+    """ 保存临时数据 """
+    if (info == "" or info == None): return
+    try:
+        with open("./files/tempPicsInfo.json", "w", encoding="utf-8") as f:
+            json.dump(info, f,default=lambda o: o.__dict__, ensure_ascii=False, indent=4)
+    except Exception as e:
+        raise e
+
+def LoadingFormantTempPicsInfo() -> List[TempPicInfoStruc]:
+    """ 读取临时数据 """
+    try:
+        with open("./files/tempPicsInfo.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+            return [TempPicInfoStruc(**item) for item in data]
+    except Exception as e:
+        raise e
---
+++ backend/App/utils/data.py
@@ -0,0 +1,15 @@
+import asyncio
+from typing import List
+from struc.software import TempPicInfoStruc
+
+data_tmp_pics:List[TempPicInfoStruc] = []
+data_lock = asyncio.Lock()
+
+async def set_pic_data(pics: List[TempPicInfoStruc]):
+    global data_tmp_pics
+    async with data_lock:
+        data_tmp_pics = pics
+
+async def get_pic_data() -> List[TempPicInfoStruc]:
+    async with data_lock:
+        return data_tmp_pics
