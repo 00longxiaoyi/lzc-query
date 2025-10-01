06ba027f9faa29090ade8bd586ddab19c81336d8
longxiaoyi <00longxiaoyi@gmail.com>
2025-10-01 13:58:10 +0800

    fixed: 修复获取用户名和群号token被固定的bug

--- backend/App/utils/getUserInfo.py
+++ backend/App/utils/getUserInfo.py
@@ -1,11 +1,13 @@
 import requests
+from utils.Token import getToken
+
 
 DEVUSER_URL = "https://appstore.api.lazycat.cloud/api/v3/admin/developer?sort=-id&page=0&size=20&user_id="
 
-# 获取用户名和群号
 def GetDevUserInfo(userID: str):
+    """ 获取用户名和群号 """
     HEADERS = {
-    "Cookie": "userToken=c2eef5eb015e44589ec77ad6acff2315"
+    "Cookie": f"userToken={getToken()}"
     }
     if (userID == "" or userID is None): return f"用户信息未知， ID为{userID}" 
 
