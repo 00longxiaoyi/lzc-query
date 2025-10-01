ef41d75e2e1f272e7b633713843f6b0cb08d6c95
longxiaoyi <00longxiaoyi@gmail.com>
2025-10-01 13:49:08 +0800

    fixed: 优化整体逻辑，添加爱下载图片的功能

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,66 +1,73 @@
 <script setup>
-  import { ref, watchEffect } from 'vue'
-  import { useRequest } from "./utils/useRequest.js"
-  
-  import Header from "./components/Header.vue"
-  import TBC from "./components/TableContent.vue"
-  import CON from "./components/Content.vue"
-  import GlobalToast from "./components/GlobalToast.vue"
-  import Settings from "./components/Settings.vue"
-  import { pushMessage } from "./utils/toastStore.js"
+import { ref, watchEffect } from 'vue'
+import { useRequest } from "./utils/useRequest.js"
 
-  const selectedCom = ref(0)
-  const contentData = ref()
-  const tableData = ref([])
-  var isLoadingData = ref()
+import Header from "./components/Header.vue"
+import TBC from "./components/TableContent.vue"
+import CON from "./components/Content.vue"
+import GlobalToast from "./components/GlobalToast.vue"
+import Settings from "./components/Settings.vue"
+import { pushMessage } from "./utils/toastStore.js"
+import downloadZip from "./utils/downloadZip.js"
+import { contentWebSocket } from "./utils/webSocketContent.js"
 
-  const selectComponent = async(data) => {
-    const { type, index } = data
-    selectedCom.value = index
-    if (type == "software") {
-      if (index == 1) {
-        await setData(tableData, [], useRequest("/get_soft_ware_pending_data"))
-      }
-      if (index == 2) {
-        await setData(contentData, "", useRequest("/get_soft_ware_data"))
-      }
-    } else {
-      if (index == 1) {
-        await setData(tableData, [], useRequest("/get_article_pending_data"))
-      }
-      if (index == 2) {
-        await setData(contentData, "", useRequest("/get_article_data"))
-      }
+const selectedCom = ref(0)
+const contentData = ref()
+const tableData = ref([])
+var isLoadingData = ref()
+var isDownloadingPic = ref(true)
+
+const selectComponent = async (data) => {
+  const { type, index } = data
+  const oldIndex = selectedCom.value
+  selectedCom.value = index
+  if (type == "software") {
+    if (index == 1) {
+      await setData(tableData, [], useRequest("/get_soft_ware_pending_data"))
+    }
+    if (index == 2) {
+      // 获取所有应用数据
+      await setData(contentData, "", useRequest("/get_soft_ware_data"))
+      contentWebSocket(isDownloadingPic)
+    }
+    if (index == 3) {
+      // dom保持不变
+      selectedCom.value = oldIndex 
+      // 获取图片数据
+      await downloadZip()
+    }
+  } else {
+    if (index == 1) {
+      await setData(tableData, [], useRequest("/get_article_pending_data"))
+    }
+    if (index == 2) {
+      await setData(contentData, "", useRequest("/get_article_data"))
     }
   }
-  // const softWarePendingData = useRequest("/api/getSoftWarePendingData")
-  // const softwareData = useRequest("/api/getSoftWareData")
-
-  const setData = async (setDataVar, setDataVarDefault, getDataFun) => {
+}
+const setData = async (setDataVar, setDataVarDefault, getDataFun) => {
+  setDataVar.value = setDataVarDefault
+  const getData = getDataFun
+  const { error, loading, data, run } = getData
+  isLoadingData = loading
+  await run()
+  if (error.value != null) {
+    pushMessage(error.value, "error")
     setDataVar.value = setDataVarDefault
-    const getData = getDataFun
-    const {error, loading, data, run} = getData
-    isLoadingData = loading
-    await run() 
-    if (error.value != null) {
-      pushMessage(error.value, "error")
-      setDataVar.value = setDataVarDefault
-    } else {
-      pushMessage("数据获取成功", "success")
-      setDataVar.value = data.value
-    }
+  } else {
+    pushMessage("数据获取成功", "success")
+    setDataVar.value = data.value
   }
+}
 
- </script>
+</script>
 
 <template>
   <Settings />
-  <GlobalToast />  
-  <Header @update="selectComponent" :isLoadingData></Header>
-  <TBC v-if="selectedCom==1" :data="tableData" :isLoadingData></TBC>
-  <CON v-if="selectedCom==2" :data="contentData" :isLoadingData></CON>
+  <GlobalToast />
+  <Header @update="selectComponent" :isLoadingData :isDownloadingPic></Header>
+  <TBC v-if="selectedCom == 1" :data="tableData" :isLoadingData></TBC>
+  <CON v-if="selectedCom == 2" :data="contentData" :isLoadingData></CON>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
