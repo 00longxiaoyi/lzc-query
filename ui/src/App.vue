047359f4596f9f23e03e46c273925e590ba68e1b
longxiaoyi <00longxiaoyi@gmail.com>
2025-10-18 22:54:16 +0800

    fixed: 重构

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,24 +3,33 @@ import { ref, watchEffect } from 'vue'
 import { useRequest } from "./utils/useRequest.js"
 
 import Header from "./components/Header.vue"
+import CCN from "./components/ContentContainer.vue"
+
+
 import TBC from "./components/TableContent.vue"
 import CON from "./components/Content.vue"
+import MS from "./components/MoenySum.vue"
 import GlobalToast from "./components/GlobalToast.vue"
 import Settings from "./components/Settings.vue"
 import { pushMessage } from "./utils/toastStore.js"
 import downloadZip from "./utils/downloadZip.js"
 import { contentWebSocket } from "./utils/webSocketContent.js"
 
-const selectedCom = ref(0)
+import './themes.css'
+
+const selectedCom = ref()
 const contentData = ref()
 const tableData = ref([])
 var isLoadingData = ref()
 var isDownloadingPic = ref(true)
+const selectType = ref("")
 
 const selectComponent = async (data) => {
   const { type, index } = data
   const oldIndex = selectedCom.value
   selectedCom.value = index
+  selectType.value = type
+  console.log(selectType.value)
   if (type == "software") {
     if (index == 1) {
       await setData(tableData, [], useRequest("/get_soft_ware_pending_data"))
@@ -28,23 +37,26 @@ const selectComponent = async (data) => {
     if (index == 2) {
       // 获取所有应用数据
       await setData(contentData, "", useRequest("/get_soft_ware_data"))
-      contentWebSocket(isDownloadingPic)
+      // contentWebSocket(isDownloadingPic)
     }
     if (index == 3) {
       // dom保持不变
-      selectedCom.value = oldIndex 
+      selectedCom.value = oldIndex
       // 获取图片数据
       await downloadZip()
     }
-  } else {
+  } else if (type == "article") {
     if (index == 1) {
       await setData(tableData, [], useRequest("/get_article_pending_data"))
     }
     if (index == 2) {
       await setData(contentData, "", useRequest("/get_article_data"))
+    } else {
+
     }
   }
 }
+
 const setData = async (setDataVar, setDataVarDefault, getDataFun) => {
   setDataVar.value = setDataVarDefault
   const getData = getDataFun
@@ -63,11 +75,57 @@ const setData = async (setDataVar, setDataVarDefault, getDataFun) => {
 </script>
 
 <template>
-  <Settings />
-  <GlobalToast />
-  <Header @update="selectComponent" :isLoadingData :isDownloadingPic></Header>
-  <TBC v-if="selectedCom == 1" :data="tableData" :isLoadingData></TBC>
-  <CON v-if="selectedCom == 2" :data="contentData" :isLoadingData></CON>
+  <div class="app bright-theme">
+    <div class="title">
+      <h1>每日审核数据获取</h1>
+    </div>
+
+    <div class="main">
+
+      <!-- <Settings /> -->
+      <!-- <GlobalToast /> -->
+
+      <Header class="main-header" @update="selectComponent" :isLoadingData :isDownloadingPic></Header>
+      <CCN class="main-content"></CCN>
+    </div>
+
+    <div v-if="false">
+      <div v-if="selectType == 'other'">
+        <MS v-if="selectComponent == 1"></MS>
+      </div>
+      <div v-else>
+        <TBC v-if="selectedCom == 1" :data="tableData" :isLoadingData></TBC>
+        <CON v-if="selectedCom == 2" :data="contentData" :isLoadingData></CON>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  padding: 0 10vw;
+}
+
+.title h1 {
+  font-size: 30px;
+  color: #333;
+}
+
+.main {
+  display: flex;
+  height: 100%;
+  width: 100%;
+}
+
+.main-header {
+  width: 200px;
+}
+
+.main-content {
+  flex: 1;
+  min-width: 0;
+  min-height: 0;
+}
+</style>
